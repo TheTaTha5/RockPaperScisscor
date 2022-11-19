2aefdba01bc1d652f972545e58b0595f2d356247
Kamin <ta-zaza2012@hotmail.com>
2022-11-20 03:47:06 +0700

    foundation of project

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+// click and store user answer
+// computer random choice
+// store com == choice
+// choice compare
+// if user === rock and com === paper {
+//  print(win)} elseif {
+//  user === rock && com === rock {
+//  print(draw)} else {print(lose)}
